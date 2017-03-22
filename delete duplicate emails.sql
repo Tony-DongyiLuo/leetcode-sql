f711d5ab84f184eca44ef899d33cc53b2c940193
Tony Luo <luodongyi_career@163.com>
2017-03-22 12:09:30 +0800

    add delete duplicate emails.sql

---
+++ delete duplicate emails.sql
@@ -0,0 +1,25 @@
+/*
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+*/
+delete b.* 
+from Person a, Person b
+where a.Email = b.Email
+and b.Id > a.Id
+;
