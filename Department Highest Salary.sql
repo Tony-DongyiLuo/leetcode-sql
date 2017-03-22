fc1088c20346dd9c6015bd0e920be246a3830e1a
Tony Luo <luodongyi_career@163.com>
2017-03-22 16:48:05 +0800

    department highest salary.sql

---
+++ Department Highest Salary.sql
@@ -0,0 +1,38 @@
+/*
+The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, Max has the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+
+*/
+select d.Name as Department, e.Name as Employee, e.Salary
+from Employee e, Department d, 
+(select a.DepartmentId, max(a.Salary) as Salary 
+from Employee a 
+GROUP BY a.DepartmentId) f
+where d.Id = f.DepartmentId
+and e.DepartmentId = f.DepartmentId
+and e.Salary = f.Salary
+ORDER BY e.Salary desc
+; 
